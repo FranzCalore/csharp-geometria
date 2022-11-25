f8dfca5ae441c37ffe819697bcc13a4b21838ddf
FranzCalore <franzkaiser1993@gmail.com>
2022-11-25 14:11:56 +0100

    cominciato a impostare l'esercizio

---
+++ Geometria/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+        public int baseRettangolo;
+        public int altezzaRettangolo;
+
+        public int calcolaPerimetro() {
+            int perimetro=baseRettangolo*2 + altezzaRettangolo*2;
+            return perimetro;
+        }
+        public int calcolaArea()
+        {
+            int area = baseRettangolo * altezzaRettangolo;
+            return area;
+        }
+    }
+}
--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Geometria;
+
+Rettangolo rettangolo1= new Rettangolo();
+rettangolo1.baseRettangolo = 20;
+rettangolo1.altezzaRettangolo = 10;
+
+Console.WriteLine(rettangolo1.calcolaArea());
+Console.WriteLine(rettangolo1.calcolaPerimetro());
+
+
+StampaRettangolo(rettangolo1);
+
+
+
+
+
+
+
+
+
+
+
+// ---------- FUNCTIONS -----------
+
+
+void StampaRettangolo(Rettangolo rettangologenerico)
+{
+    Console.WriteLine("----------" + rettangologenerico + "----------");
+}
